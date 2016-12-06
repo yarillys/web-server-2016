6d1c91e41177c7d5f1149bc5b2326120974fdf8a
Yarillys <yarillys.torres@gmail.com>
2016-12-06 19:06:20 -0400

    Project setup

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
